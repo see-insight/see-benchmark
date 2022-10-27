a1ee31b6441bad1b34ca5b5d2472b39fd026ecbe
Dirk Colbry <colbyrdi@msu.edu>
2022-10-27 13:45:25 -0400

    adding in first benchmark

---
+++ runExamples.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export IMGNUM=$1
+export PYTHONPATH="./see-segment/"
+
+if [ ! -f imagefile.txt ]
+then
+	touch imagefile.txt
+
+        for GT in ./see-segment/Image_data/Examples/*GT*
+        do
+	    ROOTNAME=`echo $GT | sed -e "s/_GT.*$//"`
+	    echo `echo ${ROOTNAME}.*` $GT >> imagefile.txt
+        done
+fi
+MINE=`head -n $1 imagefile.txt | tail -n 1`
+
+python RunSearch.py --seed $2 $MINE
+
+echo "FINNISHED RUNNING SCRIPT"
