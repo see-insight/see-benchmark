bb4e7d1aa3608856bc14e1c43ddf81213fcd2d6c
Dirk Colbry <colbyrdi@msu.edu>
2022-11-18 15:39:54 -0500

    Adding in testrunlocal.sh for testing locally on a dev node

---
+++ testrunlocal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+export SLURM_ARRAY_TASK_ID=$1
+
+export SLURM_JOB_ID=$2
+
+
+if [ "$1" == "" ]
+then
+    export SLURM_ARRAY_TASK_ID=1
+fi
+
+if [ "$2" == "" ]
+then
+    export SLURM_JOB_ID=2
+fi
+
+
+source ./continuousrun.sb
