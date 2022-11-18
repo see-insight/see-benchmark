8eb3617100a3e25927d16e45faaaec57b7e82f37
Dirk Colbry <colbyrdi@msu.edu>
2022-11-18 16:46:37 -0500

    Adding logic to make reinstalls faster

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,9 +4,6 @@
 # Output is a centos7.sif image with see-segment, jupyter and everything installed.
 # Secondary output is an overlay.img file you can use with OnDemand
 
-#Create base overlay
-create_jupyter_singularity_overlay
-
 #Clone see-segment
 if [ -d see-segment ]
 then
@@ -16,13 +13,28 @@ else
     git clone https://github.com/see-insight/see-segment.git
 fi
 
-#Update overlay with see-segment enviornment.
-overlay_exec conda env update --name base --file ./see-segment/environment.yml
+#Create base overlay
+if [ -f overlay.img ]
+then
+   echo "overlay.img file already found"
+else
+   echo "Creating overlay.img file"
+   create_jupyter_singularity_overlay
 
-#Make a local copy of centos7
-singularity build centos7.sif /opt/software/CentOS.container/7.4/bin/centos
+   #Update overlay with see-segment enviornment.
+   overlay_exec conda env update --name base --file ./see-segment/environment.yml
+fi
 
-#Wrap overlay.img into centos7
-singularity sif add --datatype 4 --partfs 2 --parttype 4 --partarch 2 --groupid 1 centos7.sif overlay.img
+if [ -f centos7.sif ]
+then
+   echo "Centos7.sif file already found"
+else
+   echo "Creating centos7.sif file"
 
+   #Make a local copy of centos7
+   singularity build centos7.sif /opt/software/CentOS.container/7.4/bin/centos
+
+   #Wrap overlay.img into centos7
+   singularity sif add --datatype 4 --partfs 2 --parttype 4 --partarch 2 --groupid 1 centos7.sif overlay.img
+fi
 
