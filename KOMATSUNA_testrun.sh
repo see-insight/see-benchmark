332995714be6199288f278254214f9c4a0b81aa8
Dirk Colbry <colbyrdi@msu.edu>
2022-12-27 18:35:20 -0500

    Adding KOMATSUNA single image test run to benchmark

---
+++ KOMATSUNA_testrun.sh
@@ -0,0 +1,16 @@
+
+export SLURM_ARRAY_TASK_ID=$1
+export SLURM_JOB_ID=$2
+
+if [ "$1" == "" ]
+then
+    export SLURM_ARRAY_TASK_ID=1
+fi
+
+if [ "$2" == "" ]
+then
+    export SLURM_JOB_ID=2
+fi
+
+
+source ./KOMATSUNA_Batch.sb
