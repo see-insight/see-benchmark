0ec44655d1987d5c45aee41ea4e94d88497c3e16
Dirk Colbry <colbyrdi@msu.edu>
2023-01-10 19:38:10 -0500

    fixing name again

--- EXAMPLES_CleanStart.sh
+++ EXAMPLES_CleanStart.sh
@@ -1,5 +1,5 @@
 rm ./Benchmark_Examples/*.out
 scancel `sq | grep _ | grep -v -e TIME | awk '{print $1}'`
 rm ./see-segment/Image_data/Examples/*.txt
-sbatch EXAMPLES_batch.sb 
+sbatch EXAMPLES_Batch.sb 
 
